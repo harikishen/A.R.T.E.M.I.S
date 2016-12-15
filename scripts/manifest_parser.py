6b074ef887588a5ddc7b8507d10939144e93f7d6
deonsunny17 <deonsunny123@gmail.com>
2016-12-15 16:04:08 +0530

    Manifest Parser Added

---
+++ scripts/manifest_parser.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+infile = open("manifest.txt","r")
+contents = infile.read()
+soup = BeautifulSoup(contents,'xml')
+titles = soup.find_all('uses-permission')
+for title in titles:
+    print(title.get('android:name').split('android.permission.')[1])

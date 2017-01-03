c53ff9c20f13a33adc80ae3b8407492b1e91d949
deonsunny17 <deonsunny123@gmail.com>
2017-01-03 10:28:56 +0530

    extraction

---
+++ scripts/extract.py
@@ -0,0 +1,27 @@
+import os
+import glob
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+path="/home/deon/apk/apks/"
+filenames=glob.glob(path+"*.apk")
+path2="/home/deon/Android-Malware-Detector"
+
+logger.info("extracting apks...")
+for filename in filenames:
+	dir=filename.split('/')[-1].split('.')[0]
+	os.chdir(path)
+	os.system("mkdir "+dir)
+	os.system("unzip %s.apk -d %s" %(dir,dir))
+	os.chdir(path2+"/scripts")
+	path1="%s/%s"%(path,dir)
+	os.system(" java -jar /home/deon/Android-Malware-Detector/tools/AXMLPrinter2.jar %s/AndroidManifest.xml >> %s/manifest.txt" %(path1,path1))
+	os.system("python manifest_parser.py %s"%(path1))
+
+
+
+
+
+
--- scripts/manifest_parser.py
+++ scripts/manifest_parser.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
-infile = open("manifest.txt","r")
+import sys
+
+path=sys.argv[1]
+
+infile = open(path+"/manifest.txt","r")
+file=open(path+"/permissions.txt","a")
 contents = infile.read()
 soup = BeautifulSoup(contents,'xml')
 titles = soup.find_all('uses-permission')
 for title in titles:
-    print(title.get('android:name').split('android.permission.')[1])
+    file.write(title.get('android:name').split('.')[-1]+"\n")

e2e0497c6b822f51d299e16a97673cfc76c8e3bd
harikishen <hari751995@gmail.com>
2017-01-03 22:47:06 +0530

    PEP8 Standards

--- scripts/manifest_parser.py
+++ scripts/manifest_parser.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
-infile = open("manifest.txt","r")
+
+infile = open("manifest.txt", "r")
 contents = infile.read()
-soup = BeautifulSoup(contents,'xml')
+soup = BeautifulSoup(contents, 'xml')
 titles = soup.find_all('uses-permission')
 for title in titles:
     print(title.get('android:name').split('android.permission.')[1])

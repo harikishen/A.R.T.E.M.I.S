8efac8d0cdf3e9af8e08bafae711f13274130964
deonsunny17 <deonsunny123@gmail.com>
2017-01-17 11:51:01 +0530

    dexparse changed

--- scripts/dexparse.py
+++ scripts/dexparse.py
@@ -28,33 +28,8 @@ for root, dirs, files in os.walk(appfolder+"classes-out/"):
 						m=p.search(function)
 						if m:
 							function=function.split('(')[0]
-							# api.append(classname+";->"+function+"()")
-							print(classname+";->"+function+"()")
-
-# filenames=glob.glob(path+"*.smali")
-# api=[]
-# q="\w+\(.*\)"
-# p=re.compile(q)
-
-# for filename in filenames:
-# 	with open(filename,"r") as file:
-# 		for line in file:
-# 			line=line.replace('\n',"")
-# 			line=line.replace('\t',"")
-# 			splitline=line.split(" ")[-1]
-			
-# 			if len(splitline.split(';->'))>=2:
-# 				function=splitline.split(';->')[-1]
-# 				classname=splitline.split(';->')[0]
-
-# 				classname=classname.split('/')[-1]
-
-# 				m=p.search(function)
-# 				if m:
-# 					function=function.split('(')[0]
-# 					# api.append(classname+";->"+function+"()")
-# 					print(classname+";->"+function+"()")
-
+							api.append(classname+";->"+function+"()")
+							print api
 
 
 
